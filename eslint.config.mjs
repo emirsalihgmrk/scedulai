940980761dfa5c2e455b08ca40039555ec1291cb
emirsalihgmrk <emirsalihtrbzn@gmail.com>
2026-08-02 16:26:31 +0300

    add file and variable naming restriction

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,68 @@
 import { defineConfig, globalIgnores } from "eslint/config";
+import checkFile from "eslint-plugin-check-file";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+      "check-file": checkFile,
+    },
+    rules: {
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "variable",
+          modifiers: ["const"],
+          format: ["camelCase", "UPPER_CASE"],
+          leadingUnderscore: "allow",
+        },
+        {
+          selector: ["variable", "parameter"],
+          format: ["camelCase"],
+          leadingUnderscore: "allow",
+        },
+        {
+          selector: "function",
+          format: ["camelCase", "PascalCase"],
+        },
+        {
+          selector: "typeLike",
+          format: ["PascalCase"],
+        },
+        {
+          selector: "enumMember",
+          format: ["PascalCase"],
+        },
+      ],
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+      ],
+    },
+  },
+  {
+    files: ["src/**/*.md"],
+    plugins: {
+      "check-file": checkFile,
+    },
+    processor: "check-file/eslint-processor-check-file",
+    rules: {
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.md": "KEBAB_CASE",
+        },
+      ],
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
